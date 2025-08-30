44765e5e7f99929779d989eec747e4acc76f235b
agongtri <166882196+agongtri@users.noreply.github.com>
2025-08-30 14:21:39 +0700

    Add files via upload

--- esphome/components/modbus_controller2/__init__.py
+++ esphome/components/modbus_controller2/__init__.py
@@ -1,12 +1,15 @@
-import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import modbus
-from esphome.components.modbus import CONF_MODBUS_ID   # ✅ ambil dari sini
+import esphome.codegen as cg
 from esphome.const import CONF_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL
+from esphome.core import coroutine_with_priority
 
-CODEOWNERS = [""]
+from esphome.components import modbus
+
+CODEOWNERS = ["@esphome/core"]
 
 DEPENDENCIES = ["modbus"]
+
+CONF_MODBUS_ID = "modbus_id"
 
 modbus_controller2_ns = cg.esphome_ns.namespace("modbus_controller2")
 ModbusController2 = modbus_controller2_ns.class_("ModbusController2", cg.Component)
@@ -16,18 +19,18 @@ CONFIG_SCHEMA = cv.ensure_list(
         {
             cv.GenerateID(): cv.declare_id(ModbusController2),
             cv.Required(CONF_ADDRESS): cv.int_,
-            cv.Required(CONF_MODBUS_ID): cv.use_id(ModbusComponent),
+            cv.Required(CONF_MODBUS_ID): cv.use_id(modbus.ModbusComponent),
             cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
         }
     ).extend(cv.COMPONENT_SCHEMA)
 )
 
-
+@coroutine_with_priority(40.0)
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
-    modbus_dev = await cg.get_variable(config[CONF_MODBUS_ID])
-    cg.add(var.set_modbus_id(modbus_dev))
-    cg.add(var.set_address(config[CONF_ADDRESS]))
-    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    for conf in config:
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        parent = await cg.get_variable(conf[CONF_MODBUS_ID])
+        cg.add(var.set_modbus_parent(parent))
+        cg.add(var.set_address(conf[CONF_ADDRESS]))
+        cg.add(var.set_update_interval(conf[CONF_UPDATE_INTERVAL]))
