4ca3504efed5316714ab33cbdbb89cf185bdfef5
agongtri <166882196+agongtri@users.noreply.github.com>
2025-08-30 13:25:13 +0700

    Add files via upload

--- esphome/components/modbus_controller2/modbus_controller2.cpp
+++ esphome/components/modbus_controller2/modbus_controller2.cpp
@@ -1,18 +1,29 @@
-#include "modbus_controller2.h"
-#include "esphome/core/log.h"
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/modbus/modbus.h"
 
 namespace esphome {
 namespace modbus_controller2 {
 
-static const char *const TAG = "modbus_controller2";
+class ModbusController2 : public Component {
+ public:
+  void set_address(uint8_t address) { address_ = address; }
+  void set_modbus_id(modbus::Modbus *modbus_parent) { modbus_parent_ = modbus_parent; }
+  void set_update_interval(uint32_t update_interval) { update_interval_ = update_interval; }
 
-void ModbusController2::setup() {
-  ESP_LOGI(TAG, "Setting up Modbus Controller 2...");
-}
+  void setup() override {
+    ESP_LOGI("modbus_controller2", "Setup with address %d", address_);
+  }
 
-void ModbusController2::loop() {
-  // Example: add main loop handling here
-}
+  void loop() override {
+    // TODO: implement polling / request
+  }
+
+ protected:
+  uint8_t address_;
+  modbus::Modbus *modbus_parent_;
+  uint32_t update_interval_;
+};
 
 }  // namespace modbus_controller2
 }  // namespace esphome
--- esphome/components/modbus_controller2/modbus_controller2.h
+++ esphome/components/modbus_controller2/modbus_controller2.h
@@ -1,15 +1,20 @@
-#pragma once
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
+#include "modbus_controller2.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace modbus_controller2 {
 
-class ModbusController2 : public Component {
- public:
-  void setup() override;
-  void loop() override;
-};
+static const char *const TAG = "modbus_controller2";
+
+void ModbusController2::setup() {
+  ESP_LOGI(TAG, "ModbusController2 setup at address %d", address_);
+}
+
+void ModbusController2::loop() {
+  // contoh log
+  ESP_LOGV(TAG, "Polling modbus device %d ...", address_);
+  // nanti di sini bisa ditambahkan request Modbus
+}
 
 }  // namespace modbus_controller2
 }  // namespace esphome
