359eed194b6f34a2b3845d3c0f06cd28eb49c258
agongtri <166882196+agongtri@users.noreply.github.com>
2025-08-30 13:37:42 +0700

    Add files via upload

--- esphome/components/modbus_controller2/__init__.py
+++ esphome/components/modbus_controller2/__init__.py
@@ -1,31 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL
-from esphome import core
+from esphome.components import modbus
+from esphome.const import CONF_ID, CONF_MODBUS_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ["modbus"]
-AUTO_LOAD = ["modbus"]
 
 modbus_controller2_ns = cg.esphome_ns.namespace("modbus_controller2")
 ModbusController2 = modbus_controller2_ns.class_("ModbusController2", cg.Component)
 
-CONF_MODBUS_ID = "modbus_id"
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ModbusController2),
+    cv.Required(CONF_MODBUS_ID): cv.use_id(modbus.ModbusDevice),
+    cv.Required(CONF_ADDRESS): cv.int_,
+    cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
+})
 
-CONFIG_SCHEMA = cv.All(
-    cv.ensure_list(
-        cv.Schema({
-            cv.GenerateID(): cv.declare_id(ModbusController2),
-            cv.Required(CONF_ADDRESS): cv.int_,
-            cv.Required(CONF_MODBUS_ID): cv.use_id(cg.Component),
-            cv.Optional(CONF_UPDATE_INTERVAL, default="5s"): cv.update_interval,
-        }).extend(cv.COMPONENT_SCHEMA)
-    )
-)
 
 async def to_code(config):
-    for conf in config:
-        var = cg.new_Pvariable(conf[CONF_ID])
-        await cg.register_component(var, conf)
-        cg.add(var.set_address(conf[CONF_ADDRESS]))
-        cg.add(var.set_modbus_id(conf[CONF_MODBUS_ID]))
-        cg.add(var.set_update_interval(conf[CONF_UPDATE_INTERVAL]))
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    # FIX bagian ini -> ambil variabel modbus lewat cg.get_variable
+    modbus_dev = await cg.get_variable(config[CONF_MODBUS_ID])
+    cg.add(var.set_modbus_id(modbus_dev))
+
+    cg.add(var.set_address(config[CONF_ADDRESS]))
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
